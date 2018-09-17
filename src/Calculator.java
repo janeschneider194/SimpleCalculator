b9f3fa6828b441e44bba41240634bbed30a8909f
janeschneider194 <42978529+janeschneider194@users.noreply.github.com>
2018-09-17 14:28:59 -0400

    Add files via upload

---
+++ src/Calculator.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ * @author Jane Schneider
+ *
+ */
+public class Calculator 
+{
+	
+	public int add(int num1, int num2)
+	{
+		int ans = num1 + num2;
+		return ans;
+	}
+}
---
+++ src/CalculatorName.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ * @author jane schneider
+ *
+ */
+public class CalculatorName
+{
+	public static void main (String[] args)
+	{
+		Calculator calc = new Calculator();
+		calc.add(2, 4);
+		System.out.print(calc.add(2, 4));
+		
+	}
+}
